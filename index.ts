8a3f08b9002d0deaac630dd8057c80b2092288cb
deeper-x <albertodeprezzo@gmail.com>
2019-01-02 12:26:28 +0100

    logging errors

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 /// <reference path="./reverse-with-sharps.d.ts"/>
 import * as myModule from 'reverse-with-sharps';
 
-console.log(myModule.reverseWithSharps(109283));
+console.log(myModule.reverseWithSharps("correct"));
 
+//console.log(myModule.reverseWithSharps(39457)); ## that's compilation error
